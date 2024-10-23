4ac1e582aa3cc5089357eb6981a403da3b84bf5e
Md Imrul Hassan <mihassan@gmail.com>
2024-10-24 10:47:26 +1100

    Add frontend code for scrabble.

---
+++ static/scrabble/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("scrabbleData", () => ({
+    letters: "",
+    grid: "",
+    dictionary: "Tiny",
+    sizes: ["Tiny", "Small", "Medium", "Large", "Huge", "Full"],
+    loading: false,
+
+    solve() {
+      if (this.loading) return;
+      this.loading = true;
+      fetch("../api/scrabble", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          dictionary: this.dictionary,
+          letters: this.letters,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          this.grid = data.grid.join("\n");
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error("Error solving:", error);
+          this.loading = false;
+        });
+    },
+  }));
+});
